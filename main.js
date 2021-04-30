2767968cb54dd49997d7a90e99200bd16a5c780f
Nishant2603 <83067785+Nishant2603@users.noreply.github.com>
2021-04-30 15:40:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function DT() {
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+}
+function LT() {
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+}
